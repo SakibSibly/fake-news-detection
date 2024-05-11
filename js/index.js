444ef26886643742149b99b4a2a591f2f814a131
TahmidMuntaser <tahmid25muntaser@gmail.com>
2024-05-11 12:39:42 +0600

    add jsfile

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// On page load
+window.onload = function() {
+    var toggleSidebar = localStorage.getItem('toggleSidebar');
+    var isChecked = (toggleSidebar === 'true');
+    document.getElementById('toggle-sidebar').checked = isChecked;
+    document.querySelector('.abus').classList.toggle('move-right', isChecked);
+};
+
+
+// Checkbox state change
+document.getElementById('toggle-sidebar').addEventListener('change', function() {
+    localStorage.setItem('toggleSidebar', this.checked);
+    document.querySelector('.abus').classList.toggle('move-right', this.checked);
+});
+
+// Not select the same page
+var sidebarLinks = document.querySelectorAll('.sidebar .menu a');
+for (var i = 0; i < sidebarLinks.length; i++) {
+    sidebarLinks[i].addEventListener('click', function(event) {
+        var currentUrl = window.location.href.split('#')[0];
+        var clickedUrl = this.href.split('#')[0];
+        if (currentUrl === clickedUrl) {
+            event.preventDefault();
+        }
+    });
+};
