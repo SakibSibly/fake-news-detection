697c941d4ec9e419e69bbdf0b3f8045e75aff88d
Azazul Islam <islamazazul72@gmail.com>
2024-05-21 23:49:58 +0600

    signin.js file added

---
+++ js/signin.js
@@ -0,0 +1,16 @@
+function signinn(){
+    var Mail = document.getElementById('mail').value;
+    var Pass = document.getElementById('pass').value;
+    if(Mail == ""){
+        alert('Please provide your mail first!');
+        return false;
+    }
+    else if(Pass == ""){
+        alert('Provide your Correct Password!');
+        return false;
+    }
+    else{
+        alert("Login Successfully.");
+        return true;
+    }
+}
