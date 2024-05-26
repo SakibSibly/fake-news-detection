0be0f583ff44d0620165c71f94b103261ae8bcf7
TahmidMuntaser <tahmid25muntaser@gmail.com>
2024-05-26 10:36:27 +0600

    fixing input redText error 0.1

--- js/signin.js
+++ js/signin.js
@@ -1,16 +1,25 @@
 function signinn(){
-    var Mail = document.getElementById('mail').value;
-    var Pass = document.getElementById('pass').value;
-    if(Mail == ""){
-        alert('Please provide your mail first!');
+    var Mail = document.getElementById('mail');
+    var Pass = document.getElementById('pass');
+    var mailError = document.getElementById('mail-error');
+    var passError = document.getElementById('pass-error');
+
+    if(Mail.value == ""){
+        Mail.style.borderColor = "red";
+        mailError.textContent = "This is a required field.";
         return false;
     }
-    else if(Pass == ""){
-        alert('Provide your Correct Password!');
+    else if(Pass.value  == ""){
+        Pass.style.borderColor = "red";
+        passError.textContent = "This is a required field.";
         return false;
     }
     else{
         // alert("Login Successfully.");
+        Mail.style.borderColor = "";
+        mailError.textContent = "";
+        Pass.style.borderColor = "";
+        passError.textContent = "";
         return true;
     }
 }
